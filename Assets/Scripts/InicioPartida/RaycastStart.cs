495f1d2ce84d62aaa684361b809e7e882f657013
Sokas <aernestolh@gmail.com>
2024-01-30 21:18:33 +0100

    MecanicaInicioPartida
    Comienzo de la mecanica de inicio de partida, reconoce cuando se mira a la mesa y se mueve al punto designado, esto ultimo hay que mejorarlo ya que se mueve de forma extraña

---
+++ Assets/Scripts/InicioPartida/RaycastStart.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastStart : MonoBehaviour
+{
+
+    [SerializeField] private LayerMask whatToDetect;
+    float maxDistance = 3f;
+    RaycastHit hit;
+    public GameObject player;
+    public GameObject target;
+    bool startGame = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    
+
+    void Update()
+    {
+
+        Ray ray = new Ray(transform.position, transform.forward);
+
+
+        if (Physics.Raycast(ray, out hit, maxDistance, whatToDetect))
+        {
+            Debug.DrawRay(ray.origin, ray.direction * 30f, Color.green);
+            Debug.Log("Distancia:" + hit.distance);
+            Debug.Log("Punto de impacto:" + hit.point);
+
+            if (Input.GetKey(KeyCode.E))
+            {
+                startGame = true;
+            }
+
+        } if (startGame)
+        {
+            Move();
+        }
+
+        Debug.Log(startGame);
+
+
+    }
+
+
+    void Move()
+    {
+        player.transform.position = Vector3.MoveTowards(player.transform.position, target.transform.position, Time.deltaTime);
+
+        
+        if(target.transform.position == player.transform.position) {
+            startGame = false;
+        }
+        
+    }
+}
