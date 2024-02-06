b4796cbbb3e58f3a2679b3bfd7377b8e9cf7e0ad
greendani27 <94862098+greendani27@users.noreply.github.com>
2024-02-06 19:31:00 +0100

    Add script general de bloques
    Añado la estructura básica del script, falta definir los metodos

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+
+    [SerializeField] private float mass = 1f;
+
+    [SerializeField] private bool isSlime, isHeavy, isRebound, isLastChance, isRandom;
+}
