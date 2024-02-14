40a34a1f325ca9e4f8a99b96a19f8ca1f5843e23
danii <greendani27@hotmail.com>
2024-02-14 16:25:00 +0100

    Terminado Bloque pesado
    Añadido funcionalidad al boton del bloque pesado y modifica la masa del bloque

--- Assets/Scripts/Fase construcción/DropObject.cs
+++ Assets/Scripts/Fase construcción/DropObject.cs
@@ -11,6 +11,32 @@ public class DropObject : MonoBehaviour
     void Update() {
     if(GameManager.Instance.gameState==GameState.Playing){
         if (Input.GetMouseButtonDown(0) && instantiateBlock.instance.buildingUp && instantiateBlock.instance.onTheArena && GameManager.Instance.countBlocks <= 15) {
+
+                //TODO va pero no me gusta la implementacion
+                if (block.GetComponent<Block>().isHeavy)
+                {
+                    block.GetComponent<Block>().mass = 2f;
+                }
+                else if (block.GetComponent<Block>().isLastChance)
+                {
+
+                }
+                else if (block.GetComponent<Block>().isRandom)
+                {
+
+                }
+                else if (block.GetComponent<Block>().isRebound)
+                {
+
+                }
+                else if (block.GetComponent<Block>().isSlime)
+                {
+
+                }
+                else 
+                {
+                    block.GetComponent<Block>().mass = 1f;
+                }
             Instantiate(block, instantiateBlock.instance.spawnBlock.transform.position, instantiateBlock.instance.spawnBlock.transform.rotation);
             GameManager.Instance.countBlocks += 1;
             
